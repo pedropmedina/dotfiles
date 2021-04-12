8a530a74a9ef53ed8b52d4d41d488c468a14bcb6
Pedro P. Medina <pedrodelilia@gmail.com>
2021-04-11 23:58:09 -0400

    Change status line colors

--- nvim/lua/config/statusline/galaxyline.lua
+++ nvim/lua/config/statusline/galaxyline.lua
@@ -6,67 +6,50 @@ local section = galaxyline.section
 
 galaxyline.short_line_list = { 'NvimTree', 'vista', 'dbui', 'packer' }
 
--- Constants
--- local COLORS = {
---     bg = '#282c34',
---     white = '#abb2bf',
---     black = '#282c34 ',
---     darkblue = '#242830',
---     lightyellow = '#e5c07b',
---     darkyellow = '#d19a66',
---     lightred = '#e06c75',
---     darkred = '#be5046',
---     blue = '#61afef',
---     mangenta = '#c678dd',
---     cyan = '#56b6c2',
---     green = '#98c379',
---     lightgrey = '#c0c0c0',
---     mediumgrey = '#5c6370',
---     darkgrey = '#2e323a'
--- }
-
 local COLORS = {
-    bg = '#2E3440',
-    white = '#EDE6DE',
-    black = '#282c34',
-    darkblue = '#3B4252',
-    lightyellow = '#e5c07b',
-    darkyellow = '#d19a66',
-    lightred = '#e06c75',
-    darkred = '#be5046',
-    blue = '#61afef',
-    mangenta = '#c678dd',
-    cyan = '#56b6c2',
-    green = '#8BC270',
-    lightgrey = '#D8DEE9',
-    mediumgrey = '#434C5E',
-    darkgrey = '#3B4252'
+    nord0_gui = '#2E3440',
+    nord1_gui = '#3B4252',
+    nord2_gui = '#434C5E',
+    nord3_gui = '#4C566A',
+    nord3_gui_bright = '#616E88',
+    nord4_gui = '#D8DEE9',
+    nord5_gui = '#E5E9F0',
+    nord6_gui = '#ECEFF4',
+    nord7_gui = '#8FBCBB',
+    nord8_gui = '#88C0D0',
+    nord9_gui = '#81A1C1',
+    nord10_gui = '#5E81AC',
+    nord11_gui = '#BF616A',
+    nord12_gui = '#D08770',
+    nord13_gui = '#EBCB8B',
+    nord14_gui = '#A3BE8C',
+    nord15_gui = '#B48EAD'
 }
 
 local MODE_COLORS = {
-    n = COLORS.green,
-    i = COLORS.blue,
-    c = COLORS.darkred,
-    R = COLORS.cyan,
-    t = COLORS.mangenta,
-    S = COLORS.darkyellow,
-    s = COLORS.darkyellow,
-    V = COLORS.darkyellow,
-    v = COLORS.darkyellow,
-    [''] = COLORS.darkyellow
+    n = COLORS.nord7_gui,
+    i = COLORS.nord14_gui,
+    c = COLORS.nord11_gui,
+    R = COLORS.nord9_gui,
+    t = COLORS.nord9_gui,
+    S = COLORS.nord13_gui,
+    s = COLORS.nord13_gui,
+    V = COLORS.nord13_gui,
+    v = COLORS.nord13_gui,
+    [''] = COLORS.nord13_gui
 }
 
 local MODE_TEXTS = {
-    n = ' NORMAL ',
-    i = ' INSERT ',
-    c = ' COMMAND ',
-    R = 'REPLACE',
-    t = ' TERMINAL ',
-    S = ' SELECT ',
-    s = ' SELECT ',
-    V = ' VISUAL ',
-    v = ' VISUAL ',
-    [''] = ' VISUAL '
+    n = ' • normal • ',
+    i = ' • insert • ',
+    c = ' • command • ',
+    R = ' • replace • ',
+    t = ' • terminal • ',
+    S = ' • select • ',
+    s = ' • select • ',
+    V = ' • visual • ',
+    v = ' • visual • ',
+    [''] = ' • visual •'
 }
 
 local buffer_not_empty = function()
@@ -86,22 +69,12 @@ local checkwidth = function()
     return false
 end
 
--- local has_item = function(item, list)
---   for _, i in ipairs(list) do
---     if i == item then do return true end
---     return false
---   end
--- end
--- end
-
 section.left[1] = {
     BlockIndicator = {
         provider = function()
             vim.api.nvim_command('hi GalaxyBlockIndicator guibg=' .. MODE_COLORS[vim.fn.mode()])
-            -- vim.api.nvim_command('hi GalaxyViMode guifg=' .. MODE_COLORS[vim.fn.mode()])
             return ' '
-        end,
-        highlight = { COLORS.lightgrey, COLORS.blue }
+        end
     }
 }
 
@@ -110,7 +83,7 @@ section.left[2] = {
         provider = function()
             return ' '
         end,
-        highlight = { COLORS.lightgrey, COLORS.darkgrey }
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui }
     }
 }
 
@@ -119,83 +92,78 @@ section.left[3] = {
         provider = function()
             return MODE_TEXTS[vim.fn.mode()]
         end,
-        highlight = { COLORS.white, COLORS.darkgrey }
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui }
     }
 }
 
 section.left[4] = {
-    ViModeSeparator = {
-        provider = function()
-            return '| '
-        end,
+    FileName = {
         condition = buffer_not_empty,
-        highlight = { COLORS.white, COLORS.darkgrey }
+        provider = root_path,
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui }
     }
 }
 
 section.left[5] = {
-    FileName = {
-        condition = buffer_not_empty,
-        provider = root_path,
-        separator = ' ',
-        separator_highlight = { COLORS.darkblue, COLORS.darkgrey },
-        highlight = { COLORS.white, COLORS.darkgrey }
+    ModBuffer = {
+        provider = function()
+            return vim.bo.mod and '  +  ' or ''
+        end,
+        highlight = { COLORS.nord14_gui, COLORS.nord1_gui }
     }
 }
 
 section.left[6] = {
-    ModBuffer = {
-        provider = function()
-            return vim.bo.mod and '[+] ' or ''
-        end,
-        highlight = { COLORS.green, COLORS.darkgrey }
-    }
+    DiagnosticHint = { provider = 'DiagnosticHint', icon = '• ', highlight = { COLORS.nord10_gui, COLORS.nord1_gui } }
 }
 
 section.left[7] = {
-    DiagnosticHint = { provider = 'DiagnosticHint', icon = '• ', highlight = { COLORS.white, COLORS.bg } }
-}
-
-section.left[8] = {
     DiagnosticSepartor1 = {
         provider = function()
             return ' '
-        end
+        end,
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui }
     }
+}
+
+section.left[8] = {
+    DiagnosticError = { provider = 'DiagnosticError', icon = '• ', highlight = { COLORS.nord11_gui, COLORS.nord1_gui } }
 }
 
 section.left[9] = {
-    DiagnosticError = { provider = 'DiagnosticError', icon = '• ', highlight = { COLORS.darkred, COLORS.bg } }
-}
-
-section.left[10] = {
     DiagnosticSepartor2 = {
         provider = function()
             return ' '
-        end
+        end,
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui }
     }
 }
 
-section.left[11] = {
-    DiagnosticWarn = { provider = 'DiagnosticWarn', icon = '• ', highlight = { COLORS.darkyellow, COLORS.bg } }
+section.left[10] = {
+    DiagnosticWarn = { provider = 'DiagnosticWarn', icon = '• ', highlight = { COLORS.nord12_gui, COLORS.nord1_gui } }
 }
 
 section.right[1] = {
     GitBranch = {
         provider = function()
-            return '⠕ ' .. vcs.get_git_branch()
+            return '⠕ ' .. vcs.get_git_branch() .. ' '
         end,
         condition = function()
             return buffer.get_buffer_filetype() ~= '' and cond.check_git_workspace() and vcs.get_git_branch()
         end,
         separator = ' ',
-        separator_highlight = { COLORS.darkblue, COLORS.darkgrey },
-        highlight = { COLORS.white, COLORS.darkgrey }
+        separator_highlight = { COLORS.nord1_gui, COLORS.nord2_gui },
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord2_gui }
     }
 }
 
 section.right[2] = {
-    DiffAdd = { provider = 'DiffAdd', condition = checkwidth, icon = '+', highlight = { COLORS.green, COLORS.darkgrey } }
+    DiffAdd = {
+        provider = 'DiffAdd',
+        condition = checkwidth,
+        icon = ' +',
+        highlight = { COLORS.nord14_gui, COLORS.nord2_gui }
+    }
 }
 
 section.right[3] = {
@@ -203,7 +171,7 @@ section.right[3] = {
         provider = 'DiffModified',
         condition = checkwidth,
         icon = '~',
-        highlight = { COLORS.darkyellow, COLORS.darkgrey }
+        highlight = { COLORS.nord12_gui, COLORS.nord2_gui }
     }
 }
 
@@ -212,7 +180,7 @@ section.right[4] = {
         provider = 'DiffRemove',
         condition = checkwidth,
         icon = '-',
-        highlight = { COLORS.darkred, COLORS.darkgrey }
+        highlight = { COLORS.nord11_gui, COLORS.nord2_gui }
     }
 }
 
@@ -222,9 +190,9 @@ section.right[5] = {
             return buffer.get_buffer_filetype() ~= '' and cond.check_git_workspace()
         end,
         provider = function()
-            return ' |'
+            return ' •'
         end,
-        highlight = { COLORS.darkblue, COLORS.darkgrey }
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord2_gui }
     }
 }
 
@@ -232,17 +200,17 @@ section.right[6] = {
     FileFormat = {
         provider = 'FileFormat',
         separator = ' ',
-        separator_highlight = { COLORS.darkblue, COLORS.darkgrey },
-        highlight = { COLORS.lightgrey, COLORS.darkgrey }
+        separator_highlight = { COLORS.nord1_gui, COLORS.nord2_gui },
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord2_gui }
     }
 }
 
 section.right[7] = {
     LineInfo = {
         provider = 'LineColumn',
-        separator = ' | ',
-        separator_highlight = { COLORS.darkblue, COLORS.darkgrey },
-        highlight = { COLORS.lightgrey, COLORS.darkgrey }
+        separator = ' • ',
+        separator_highlight = { COLORS.nord3_gui_bright, COLORS.nord2_gui },
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord2_gui }
     }
 }
 
@@ -251,7 +219,7 @@ section.right[8] = {
         provider = function()
             return ' '
         end,
-        highlight = { COLORS.lightgrey, COLORS.darkgrey }
+        highlight = { COLORS.nord1_gui, COLORS.nord2_gui }
     }
 }
 
@@ -259,8 +227,8 @@ section.right[9] = {
     PerCent = {
         provider = 'LinePercent',
         separator = ' ',
-        separator_highlight = { COLORS.darkblue, COLORS.darkblue },
-        highlight = { COLORS.lightgrey, COLORS.darkblue }
+        separator_highlight = { COLORS.nord1_gui, COLORS.nord1_gui },
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui }
     }
 }
 
@@ -269,7 +237,7 @@ section.short_line_left[1] = {
         provider = function()
             return ' '
         end,
-        highlight = { COLORS.darkblue, COLORS.darkblue }
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui }
     }
 }
 
@@ -279,7 +247,7 @@ section.short_line_left[2] = {
             return buffer.get_buffer_filetype() ~= '' and buffer.get_buffer_filetype() or 'VIM'
         end,
         separator = '',
-        separator_highlight = { COLORS.white, COLORS.darkblue },
-        highlight = { COLORS.white, COLORS.darkblue }
+        separator_highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui },
+        highlight = { COLORS.nord3_gui_bright, COLORS.nord1_gui }
     }
 }
