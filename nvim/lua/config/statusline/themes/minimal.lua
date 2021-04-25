4cebc2700a4bb8c3b91f72cda0e179450acbbc5b
Pedro P. Medina <pedrodelilia@gmail.com>
2021-04-25 14:52:15 -0400

    Add statusline/utils and cleanup themes/minimal

--- nvim/lua/config/statusline/themes/minimal.lua
+++ nvim/lua/config/statusline/themes/minimal.lua
@@ -1,67 +1,20 @@
-local galaxyline = require('galaxyline')
+local gl = require('galaxyline')
 local gl_buffer = require('galaxyline.provider_buffer')
 local gl_vcs = require('galaxyline.provider_vcs')
-local gl_cond = require('galaxyline.condition')
-local gl_section = galaxyline.section
 local gl_diagnostic = require('galaxyline.provider_diagnostic')
 local gl_fileinfo = require('galaxyline.provider_fileinfo')
+local gl_section = gl.section
+
 local COLORS = require('config.statusline.colors').NORD
 local modes = require('config.statusline.modes')
+local utils = require('config.statusline.utils')(COLORS)
 
 local MODE_COLORS = modes.mode_colors(COLORS)
 local MODE_TEXTS = modes.mode_texts()
 
-galaxyline.short_line_list = { 'NvimTree', 'vista', 'dbui', 'packer' }
+gl.short_line_list = { 'NvimTree', 'vista', 'dbui', 'packer' }
 
-local buffer_not_empty = function()
-    if vim.fn.empty(vim.fn.expand('%:t')) ~= 1 then return true end
-    return false
-end
-
--- Get project's root dit based off .git or fallback to ~
-local root_path = function()
-    local git_dir_path = gl_vcs.get_git_dir(vim.fn.expand('%:p:h'))
-    return not git_dir_path and vim.fn.expand('%:~') or '...' .. vim.fn.expand('%:p'):sub(git_dir_path:len() - 4)
-end
-
-local checkwidth = function()
-    local squeeze_width = vim.fn.winwidth(0) / 2
-    if squeeze_width > 40 then return true end
-    return false
-end
-
-local set_section = function(name, provider, condition, icon, fg, bg)
-    local provider_cb = function(p)
-        return function()
-            return p and p or ' '
-        end
-    end
-
-    local prov = not provider and provider_cb() or type(provider) == 'function' and provider or provider_cb(provider)
-
-    return {
-        [name] = {
-            provider = prov,
-            condition = function()
-                if (condition) then return condition() end
-                return true
-            end,
-            icon = icon or nil,
-            highlight = { fg or COLORS.darkest, bg or COLORS.darkest }
-        }
-    }
-end
-
-local cond = {
-    branch = function()
-        return gl_buffer.get_buffer_filetype() ~= '' and gl_cond.check_git_workspace() and gl_vcs.get_git_branch()
-    end,
-    git_separator = function()
-        return gl_buffer.get_buffer_filetype() ~= '' and gl_cond.check_git_workspace()
-    end
-}
-
-local prov = {
+local providers = {
     block_indicator = function()
         vim.api.nvim_command('hi GalaxyBlockIndicator guibg=' .. MODE_COLORS[vim.fn.mode()])
         return ' '
@@ -81,37 +34,40 @@ local prov = {
 }
 
 local left = {
-    set_section('BlockIndicator', prov.block_indicator),
-    set_section('Separator_1'),
-    set_section('ViMode', prov.vi_mode, nil, nil, COLORS.medium),
-    set_section('Separator_2'),
-    set_section('FileName', root_path, buffer_not_empty, nil, COLORS.medium),
-    set_section('Separator_3'),
-    set_section('ModBuffer', prov.dirty_buf, nil, nil, COLORS.success),
-    set_section('Separator_4'),
-    set_section('DiagnosticHint', gl_diagnostic.get_diagnostic_hint, nil, '• ', COLORS.light),
-    set_section('Separator_5'),
-    set_section('DiagnosticError', gl_diagnostic.get_diagnostic_error, nil, '• ', COLORS.error),
-    set_section('Separator_6'),
-    set_section('DiagnosticWarn', gl_diagnostic.get_diagnostic_warn, nil, '• ', COLORS.warning)
+    utils.set_section('BlockIndicator', providers.block_indicator),
+    utils.set_section('Separator_1'),
+    utils.set_section('ViMode', providers.vi_mode, nil, nil, COLORS.medium),
+    utils.set_section('Separator_2'),
+    utils.set_section('FileName', utils.root_path, utils.buffer_not_empty, nil, COLORS.medium),
+    utils.set_section('Separator_3'),
+    utils.set_section('ModBuffer', providers.dirty_buf, nil, nil, COLORS.success),
+    utils.set_section('Separator_4'),
+    utils.set_section('DiagnosticHint', gl_diagnostic.get_diagnostic_hint, nil, '• ', COLORS.light),
+    utils.set_section('Separator_5'),
+    utils.set_section('DiagnosticError', gl_diagnostic.get_diagnostic_error, nil, '• ', COLORS.error),
+    utils.set_section('Separator_6'),
+    utils.set_section('DiagnosticWarn', gl_diagnostic.get_diagnostic_warn, nil, '• ', COLORS.warning)
 }
 
 local right = {
-    set_section('GitBranch', prov.branch, cond.branch, nil, COLORS.medium),
-    set_section('Separator_7'),
-    set_section('DiffAdd', gl_vcs.diff_add, checkwidth, '+', COLORS.success),
-    set_section('DiffModified', gl_vcs.diff_modified, checkwidth, '~', COLORS.warning),
-    set_section('DiffRemove', gl_vcs.diff_remove, checkwidth, '-', COLORS.error),
-    set_section('Separator_8', ' • ', cond.git_separator, nil, COLORS.medium),
-    set_section('FileFormat', gl_buffer.get_buffer_filetype, nil, nil, COLORS.medium),
-    set_section('Separator_9', '  • ', nil, nil, COLORS.medium),
-    set_section('LineInfo', gl_fileinfo.line_column, nil, nil, COLORS.medium),
-    set_section('Separator_10', ' • ', nil, nil, COLORS.medium),
-    set_section('PerCent', gl_fileinfo.current_line_percent, nil, nil, COLORS.medium),
-    set_section('Separator_11', ' ', nil)
+    utils.set_section('GitBranch', providers.branch, utils.has_git_branch, nil, COLORS.medium),
+    utils.set_section('Separator_7'),
+    utils.set_section('DiffAdd', gl_vcs.diff_add, utils.checkwidth, '+', COLORS.success),
+    utils.set_section('DiffModified', gl_vcs.diff_modified, utils.checkwidth, '~', COLORS.warning),
+    utils.set_section('DiffRemove', gl_vcs.diff_remove, utils.checkwidth, '-', COLORS.error),
+    utils.set_section('Separator_8', ' • ', utils.has_git, nil, COLORS.medium),
+    utils.set_section('FileFormat', gl_buffer.get_buffer_filetype, nil, nil, COLORS.medium),
+    utils.set_section('Separator_9', '  • ', nil, nil, COLORS.medium),
+    utils.set_section('LineInfo', gl_fileinfo.line_column, nil, nil, COLORS.medium),
+    utils.set_section('Separator_10', ' • ', nil, nil, COLORS.medium),
+    utils.set_section('PerCent', gl_fileinfo.current_line_percent, nil, nil, COLORS.medium),
+    utils.set_section('Separator_11', ' ', nil)
 }
 
-local short_left = { set_section('Separator_12'), set_section('BufferType', prov.buffer_type, nil, nil, COLORS.medium) }
+local short_left = {
+    utils.set_section('Separator_12'),
+    utils.set_section('BufferType', providers.buffer_type, nil, nil, COLORS.medium)
+}
 
 local set_section_position = function(position, config)
     for i, table in ipairs(config) do gl_section[position][i] = table end
