d12816efaa99ae79dcfd28726105d221548a1940
Pedro P. Medina <pedrodelilia@gmail.com>
2021-07-08 14:31:19 -0400

    Minor changes to tree

--- nvim/lua/config/tree/init.lua
+++ nvim/lua/config/tree/init.lua
@@ -36,7 +36,7 @@ return function()
     vim.g.nvim_tree_hijack_netrw = 1
 
     -- This is the default. See :help filename-modifiers for more options
-    vim.g.nvim_tree_root_folder_modifier = ':~'
+    vim.g.nvim_tree_root_folder_modifier = ':t'
 
     -- 0 by default, will open the tree when entering a new tab and the tree was previously open
     vim.g.nvim_tree_tab_open = 0
@@ -52,6 +52,9 @@ return function()
 
     --  select which icons to show
     vim.g.nvim_tree_show_icons = { git = 0, files = 1, folders = 1 }
+
+    -- 0 by default, will update the tree cwd when changing nvim's directory (DirChanged event). Behaves strangely with autochdir set.
+    vim.g.nvim_tree_update_cwd = 1
 
     local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
