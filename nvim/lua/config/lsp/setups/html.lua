bca37d9387fe241ee9597a67eea0f239a68ec749
Pedro P. Medina <pedrodelilia@gmail.com>
2021-07-08 22:47:57 -0400

    Nothing major

--- nvim/lua/config/lsp/setups/html.lua
+++ nvim/lua/config/lsp/setups/html.lua
@@ -1,7 +1,17 @@
 -- https://github.com/vscode-langservers/vscode-html-languageserver-bin
 local html = function(config)
     local opts = {
-        filetypes = { 'html', 'tsx', 'jsx', 'vue', 'hbs', 'handlebars', 'html.hbs', 'html.handlebars', 'liquid' }
+        filetypes = {
+            'html',
+            'tsx',
+            'jsx',
+            'vue',
+            'hbs',
+            'handlebars',
+            'html.hbs',
+            'html.handlebars',
+            'liquid'
+        }
     }
     for k, v in pairs(config) do opts[k] = v end
     return opts
