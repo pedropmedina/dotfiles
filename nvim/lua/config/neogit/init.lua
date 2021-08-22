66a7526a7d6f5028e786fc69941eccdd648eeabe
Pedro P. Medina <pedrodelilia@gmail.com>
2021-08-21 22:34:29 -0400

    Remove neogit and diffview

--- nvim/lua/config/neogit/init.lua
+++
@@ -1,9 +0,0 @@
-return function()
-    require('neogit').setup {
-        disable_signs = false,
-        disable_context_highlighting = false,
-        disable_commit_confirmation = false,
-        signs = { section = { '+', '-' }, item = { '+', '-' }, hunk = { '', '' } },
-        integrations = { diffview = true }
-    }
-end
