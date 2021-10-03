ead3453d343206d0e8f100ac8fa7aab8b2814ccf
Pedro P. Medina <pedrodelilia@gmail.com>
2021-10-03 18:03:41 -0400

    Update configs for nvim tree

--- nvim/lua/config/tree/init.lua
+++ nvim/lua/config/tree/init.lua
@@ -1,24 +1,5 @@
 return function()
-    -- left by default
-    vim.g.nvim_tree_side = 'left'
-
-    -- 30 by default
-    vim.g.nvim_tree_width = 37
-
-    -- empty by default
-    vim.g.nvim_tree_ignore = { '.git', 'node_modules', '.cache' }
-
-    -- 0 by default, opens the tree when typing `vim $DIR` or `vim`
-    vim.g.nvim_tree_auto_open = 0
-
-    -- 0 by default, closes the tree when it's the last window
-    vim.g.nvim_tree_auto_close = 1
-
-    -- 0 by default, closes the tree when you open a file
-    vim.g.nvim_tree_quit_on_open = 0
-
-    -- 0 by default, this option allows the cursor to be updated when entering a buffer
-    vim.g.nvim_tree_follow = 1
+    local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
     -- 0 by default, this option shows indent markers when folders are open
     vim.g.nvim_tree_indent_markers = 1
@@ -29,43 +10,11 @@ return function()
     -- 0 by default, will enable file highlight for git attributes (can be used without the icons).
     vim.g.nvim_tree_git_hl = 0
 
-    -- 1 by default, disables netrw
-    vim.g.nvim_tree_disable_netrw = 1
-
-    -- 1 by default, prevents netrw from automatically opening when opening directories (but lets you keep its other utilities)
-    vim.g.nvim_tree_hijack_netrw = 1
-
     -- This is the default. See :help filename-modifiers for more options
     vim.g.nvim_tree_root_folder_modifier = ':t'
 
-    -- 0 by default, will open the tree when entering a new tab and the tree was previously open
-    vim.g.nvim_tree_tab_open = 0
-
-    -- 0 by default, will not resize the tree when opening a file
-    vim.g.nvim_tree_allow_resize = 1
-
-    -- 0 by default, will show lsp diagnostics in the signcolumn. See :help nvim_tree_lsp_diagnostics
-    vim.g.nvim_tree_lsp_diagnostics = 0
-
-    -- 0 by default, will not resize the tree when opening a file
-    vim.g.nvim_tree_disable_keybindings = 0
-
     --  select which icons to show
     vim.g.nvim_tree_show_icons = { git = 0, files = 1, folders = 1 }
-
-    -- 0 by default, will update the tree cwd when changing nvim's directory (DirChanged event). Behaves strangely with autochdir set.
-    vim.g.nvim_tree_update_cwd = 1
-
-    local tree_cb = require('nvim-tree.config').nvim_tree_callback
-
-    --  modify some of the key mappings
-    vim.g.nvim_tree_bindings = {
-        { key = 'l', cb = tree_cb('edit') },
-        { key = '<C-s>', cb = tree_cb('split') },
-        { key = 'h', cb = tree_cb('close_node') },
-        { key = '!', cb = tree_cb('toggle_ignored') },
-        { key = '.', cb = tree_cb('toggle_dotfiles') }
-    }
 
     --  icons to be used
     vim.g.nvim_tree_icons = {
@@ -80,5 +29,76 @@ return function()
         },
         folder = { default = '+', open = '-', empty = '+', empty_open = '-' },
         lsp = { hint = '•', info = '•', warning = '•', error = '•' }
+    }
+
+    require('nvim-tree').setup {
+        -- disables netrw completely
+        disable_netrw = true,
+        -- hijack netrw window on startup
+        hijack_netrw = true,
+        -- open the tree when running this setup function
+        open_on_setup = false,
+        -- will not open on setup if the filetype is in this list
+        ignore_ft_on_setup = {},
+        -- closes neovim automatically when the tree is the last **WINDOW** in the view
+        auto_close = false,
+        -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
+        open_on_tab = false,
+        -- hijacks new directory buffers when they are opened.
+        update_to_buf_dir = {
+            -- enable the feature
+            enable = true,
+            -- allow to open the tree if it was previously closed
+            auto_open = true
+        },
+        -- hijack the cursor in the tree to put it at the start of the filename
+        hijack_cursor = false,
+        -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
+        update_cwd = true,
+        -- show lsp diagnostics in the signcolumn
+        lsp_diagnostics = false,
+        -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
+        update_focused_file = {
+            -- enables the feature
+            enable = true,
+            -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
+            -- only relevant when `update_focused_file.enable` is true
+            update_cwd = false,
+            -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
+            -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
+            ignore_list = { '.git', 'node_modules', '.cache' }
+        },
+        -- configuration options for the system open command (`s` in the tree by default)
+        system_open = {
+            -- the command to run this, leaving nil should work in most cases
+            cmd = nil,
+            -- the command arguments as a list
+            args = {}
+        },
+
+        view = {
+            -- width of the window, can be either a number (columns) or a string in `%`, for left or right side placement
+            width = 30,
+            -- height of the window, can be either a number (columns) or a string in `%`, for top or bottom side placement
+            height = 30,
+            -- side of the tree, can be one of 'left' | 'right' | 'top' | 'bottom'
+            side = 'left',
+            -- if true the tree will resize itself after opening a file
+            auto_resize = true,
+            mappings = {
+                -- custom only false will merge the list with the default mappings
+                -- if true, it will only use your list to set the mappings
+                custom_only = false,
+                -- list of mappings to set on the tree manually
+                list = {
+                    { key = 'l', cb = tree_cb('edit') },
+                    { key = '<C-s>', cb = tree_cb('split') },
+                    { key = 'h', cb = tree_cb('close_node') },
+                    { key = '!', cb = tree_cb('toggle_ignored') },
+                    { key = '.', cb = tree_cb('toggle_dotfiles') }
+                }
+            }
+        }
+
     }
 end
