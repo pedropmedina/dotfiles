c6d1dae741a1c2442bae542eba148d2717bfb9ec
Pedro P. Medina <pedrodelilia@gmail.com>
2022-04-30 17:50:34 -0400

    Remove <CR> keymap from cmp and border from telescope

--- nvim/lua/config/completion/init.lua
+++ nvim/lua/config/completion/init.lua
@@ -17,7 +17,6 @@ if present then
 			["<Down>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
 			["<Up>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
 			["<C-y>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Replace }),
-			-- ["<CR>"] = cmp.mapping.confirm({ select = true, behavior = cmp.ConfirmBehavior.Insert }),
 			["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
 			["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
 			["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
