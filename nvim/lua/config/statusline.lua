35bd74f0efe5922644e275f537e2da18e2bed260
Pedro P. Medina <pedrodelilia@gmail.com>
2022-08-10 11:18:14 -0400

    Activate lualine on BufRead

--- nvim/lua/config/statusline.lua
+++ nvim/lua/config/statusline.lua
@@ -1,6 +1,14 @@
 local is_lualine_present, lualine = pcall(require, "lualine")
 local colors = require("colors")
 
+-- TODO: Improve util and move somewhere else
+local setup_on_buf_read = function(callback)
+	vim.api.nvim_create_autocmd({ "BufRead" }, {
+		pattern = { "*" },
+		callback = callback,
+	})
+end
+
 local theme = {
 	normal = {
 		a = { fg = colors.dark_black, bg = colors.blue },
@@ -52,49 +60,53 @@ local theme = {
 	},
 }
 
-if is_lualine_present then
-	lualine.setup({
-		options = {
-			icons_enabled = true,
-			theme = theme,
-			globalstatus = true,
-			component_separators = { left = "", right = "" },
-			section_separators = { left = "", right = "" },
-			disabled_filetypes = {},
-			always_divide_middle = true,
-			padding = 1,
-		},
-		sections = {
-			lualine_a = {
-				-- {
-				-- 	"mode",
-				-- 	fmt = function()
-				-- 		return " "
-				-- 	end,
-				-- },
+local setup_lualine = function()
+	if is_lualine_present then
+		lualine.setup({
+			options = {
+				icons_enabled = true,
+				theme = theme,
+				globalstatus = true,
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+				disabled_filetypes = {},
+				always_divide_middle = true,
+				padding = 1,
 			},
-			lualine_b = {
-				{
-					"filename",
-					path = 3,
-					padding = 2,
-					symbols = { modified = " [+]", readonly = " [-]", unnamed = "[No Name]" },
+			sections = {
+				lualine_a = {
+					-- {
+					-- 	"mode",
+					-- 	fmt = function()
+					-- 		return " "
+					-- 	end,
+					-- },
 				},
+				lualine_b = {
+					{
+						"filename",
+						path = 3,
+						padding = 2,
+						symbols = { modified = " [+]", readonly = " [-]", unnamed = "[No Name]" },
+					},
+				},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = { { "diagnostics", sources = { "nvim_diagnostic" }, padding = 2 } },
+				lualine_z = { { "branch", icon = "" }, { "diff" }, { "location" }, { "progress" } },
 			},
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = { { "diagnostics", sources = { "nvim_diagnostic" }, padding = 2 } },
-			lualine_z = { { "branch", icon = "" }, { "diff" }, { "location" }, { "progress" } },
-		},
-		inactive_sections = {
-			lualine_a = { "filename" },
-			lualine_b = {},
-			lualine_c = {},
-			lualine_x = {},
-			lualine_y = {},
-			lualine_z = { "location" },
-		},
-		tabline = {},
-		extensions = { "quickfix" },
-	})
+			inactive_sections = {
+				lualine_a = { "filename" },
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = { "location" },
+			},
+			tabline = {},
+			extensions = { "quickfix" },
+		})
+	end
 end
+
+setup_on_buf_read(setup_lualine)
