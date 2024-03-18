8880276ac8f7a61608b251140bdd7bd27ad7426c
Pedro P. Medina <pedrodelilia@gmail.com>
2024-03-18 12:10:51 -0400

    fix get_signs indexing nil

--- nvim/lua/util/ui.lua
+++ nvim/lua/util/ui.lua
@@ -12,7 +12,7 @@ function M.get_signs(buf, lnum)
   ---@type Sign[]
   local signs = vim.tbl_map(function(sign)
     ---@type Sign
-    local ret = vim.fn.sign_getdefined(sign.name)[1]
+    local ret = vim.fn.sign_getdefined(sign.name)[1] or {}
     ret.priority = sign.priority
     return ret
   end, vim.fn.sign_getplaced(buf, { group = "*", lnum = lnum })[1].signs)
